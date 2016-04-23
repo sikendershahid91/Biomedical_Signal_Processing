19ac4b70c7459b9d36eca1aa5df05dc51921759d
Sikender Shahid <sikendershahid91@gmail.com>
2016-04-23 18:25:30 -0500

    Added features matrix

--- SupervisedLearning_LDA_Project/FingerMovement.m
+++ SupervisedLearning_LDA_Project/FingerMovement.m
@@ -24,8 +24,8 @@ end
 
 % wavelet Transform (WT)- extract the coeiffienct from train_set for each trial
 daubechies_WT = 'db5';
-train_WaveletCoefficentMatrix_c3 = zeros(length(train_label),7,length(train_set_c3(1,:)));
-train_WaveletCoefficentMatrix_c4 = zeros(length(train_label),7,length(train_set_c4(1,:)));
+train_WaveletCoefficentMatrix_c3 = zeros(length(train_label),6,length(train_set_c3(1,:)));
+train_WaveletCoefficentMatrix_c4 = zeros(length(train_label),6,length(train_set_c4(1,:)));
 
 % coloumn 1-6 respectively are Noise, Gamma, Beta, Alpha, Theta, Delta
 for i=1:1:length(train_label)
@@ -47,6 +47,13 @@ for i=1:1:length(train_label)
     train_WaveletCoefficentMatrix_c4(i,5, :) = wrcoef('d',C,L,daubechies_WT,5); %Theta
     train_WaveletCoefficentMatrix_c4(i,6, :) = wrcoef('a',C,L,daubechies_WT,5); %DELTA
 end
+
+featuresMatrix_alpha_c3 = zeros(length(train_label), 6, length(train_set_c3))
+featuresMatrix_beta_c3 = zeros(length(train_label), 6, length(train_set_c3))
+featuresMatrix_alpha_c4 = zeros(length(train_label), 6, length(train_set_c4))
+featuresMatrix_beta_c4 = zeros(length(train_label), 6, length(train_set_c4))
+
+
 
 figure();
 plot(t, squeeze(train_WaveletCoefficentMatrix_c3(1,4,:)));
