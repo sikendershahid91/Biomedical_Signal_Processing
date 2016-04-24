9d4033019960b24358f379cd31f4038c7393358d
Arian 2 <Arian 2>
2016-04-24 16:45:06 -0500

    whateverchanges

---
+++ SupervisedLearning_LDA_Project/log_variance.m
@@ -0,0 +1,8 @@
+function [ out_logvar ] = log_variance(input_waveform)
+  N = length(input_waveform);
+  out_logvar = 0;
+  for n = 1:1:N
+  m = exp(mean(input_waveform) + (var(input_waveform).^2)/2);
+  v = exp(2*mean(input_waveform) + var(input_waveform).^2)*(exp(var(input_waveform).^2)-1);
+  out_logvar = sqrt(log10(v/(m)^2 +1));
+end
