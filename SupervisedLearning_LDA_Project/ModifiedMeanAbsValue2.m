5d5daf8635f31dd12918c60f77178d8da0e9b9e1
Sikender Shahid <sikendershahid91@gmail.com>
2016-04-23 23:27:01 -0500

    completed method

--- SupervisedLearning_LDA_Project/ModifiedMeanAbsValue2.m
+++ SupervisedLearning_LDA_Project/ModifiedMeanAbsValue2.m
@@ -1,12 +1,15 @@
 function [ out_MMAV2 ] = ModifiedMeanAbsValue2(input_waveform)
   N = length(input_waveform);
-  weight = ones(N);
+  out_MMAV2 = 0;
   for n = 1:1:N
     if n > 0.75*N
-      weight(n) = 4*(n-N)/N;
+      weight = 4*(n-N)/N;
     elseif n < 0.25*N
-      weight(n) = 4*n/N;
+      weight = 4*n/N;
+    else
+      weight = 1;
     end
+    out_MMAV2 = out_MMAV2 + weight*abs(input_waveform(n));
   end
-  out_MMAV2 = (1/N)*sum(weight.*abs(input_waveform))
-  end
+  out_MMAV2 = (1/N)*out_MMAV2;
+end
