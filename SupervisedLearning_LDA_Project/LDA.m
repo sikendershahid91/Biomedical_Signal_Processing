ba2ee60004cf341b2cd5f8eb399dafbf1714620e
Sikender Shahid <sikendershahid91@gmail.com>
2016-04-24 19:54:17 -0500

    Creating structure

--- SupervisedLearning_LDA_Project/LDA.m
+++ SupervisedLearning_LDA_Project/LDA.m
@@ -1,4 +1,32 @@
-function [ output_model ] = LDA( input_matrix )
+function [ output_model ] = LDA( feature_matrix )
+% function: Short description
+%
+% Extended description
+sW = createScatterMatrixWeight(feature_matrix);
+sB = createScatterMatrixBetween(feature_matrix);
+
+
+end  % function
+
+function [ scatter_weight ] = createScatterMatrixWeight( in )
+% function: Short description
+%
+% Extended description
+
+end  % function
+
+function [ scatter_between_classes] = createScatterMatrixBetween( in)
+
+end
+
+function [ out_matrix ] = sortingByEigen( in )
+% function: Short description
+%
+% Extended description
+
+end  % function
+
+function [ out_subspaceY ] = TransformNewSubSpaceY( in )
 % function: Short description
 %
 % Extended description
