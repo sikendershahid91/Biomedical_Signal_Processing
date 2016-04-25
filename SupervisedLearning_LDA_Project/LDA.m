02b5b141c5d9df74685dc3a3a34c7576d19eb8fa
Arian 2 <Arian 2>
2016-04-24 22:17:27 -0500

    changed variables

--- SupervisedLearning_LDA_Project/LDA.m
+++ SupervisedLearning_LDA_Project/LDA.m
@@ -8,18 +8,18 @@ sB = createScatterMatrixBetween(feature_matrix);
 
 end  % function
 
-function [ scatter_weight ] = createScatterMatrixWeight( in )
+function [ scatter_weight ] = createScatterMatrixWeight(scatter_w )
 % function: Short description
 %S_i is the scatter matrix for every class
 % Extended description
-[~,1]=size(in(:,1:5)); %Size of in
-classes = unique(in(:,6)); %Class vector
+[~,1]=size(scatter_w(:,1:5)); %Size of in
+classes = unique(scatter_w(:,6)); %Class vector
 totalclasses = length(classes); %Number of classes
 within_scatter = zeros(131,6); %initialize within matrix
-meanofdata = mean(in(:,1:5)); %Mean of features overall
+meanofdata = mean(scatter_w(:,1:5)); %Mean of features overall
 for n = 1:totalclasses
-  class_data = find( in(:,6)==classes(n)); %Get feature for each class
-  x = in(class_data,:);
+  class_data = find( scatter_w(:,6)==classes(n)); %Get feature for each class
+  x = scatter_w(class_data,:);
 
   m = mean(x); %mean of each class
   x = x-repmat(m,length(class_data),1); %xi - mi
@@ -27,15 +27,15 @@ for n = 1:totalclasses
 end
 end  % function
 
-function [ scatter_between_classes] = createScatterMatrixBetween( in)
-  ~,1]=size(in(:,1:5)); %Size of in
-  classes = unique(in(:,6)); %Class vector
+function [ scatter_between_classes] = createScatterMatrixBetween(scatter_b)
+  ~,1]=size(scatter_b(:,1:5)); %Size of in
+  classes = unique(scatter_b(:,6)); %Class vector
   totalclasses = length(classes); %Number of classes
   between_matrix_scatter = zeros(131,6); %initialize within matrix
   meanofdata = mean(in(:,1:5)); %Mean of features overall
   for n = 1:totalclasses
-    class_data = find( in(:,6)==classes(n)); %Get feature for each class
-    x = in(class_data,:);
+    class_data = find( scatter_b(:,6)==classes(n)); %Get feature for each class
+    x = scatter_b(class_data,:);
 
     m = mean(x); %mean of each class
     x = x-repmat(m,length(class_data),1); %xi - mi
@@ -43,10 +43,9 @@ function [ scatter_between_classes] = createScatterMatrixBetween( in)
 end
 
 function [ out_matrix ] = sortingByEigen(scatter_w, scatter_b)
-% function: input of scatter-weighted Matrix and scatter-between matrix
-% calculating the eigen value and vector, the output will containe a reduced
-% matrix based on the reduced eigen vectors.
-
+% function: Short description
+%
+% Extended description
 [eigen_vector, eigen_values] = eig(inv(scatter_w)*scatter_b);
 eigen_values = diag(eigen_values);
 eigen_vector_location = zeros(length(eigen_values),1);
@@ -56,14 +55,13 @@ end
 eigen_values_dictionary = containers.Map(eigen_values, eigen_vector_location);
 eigen_values = sort(eigen_values, 'descend');
 max_eigen_vector = eigen_vector_dictionary(eigen_values(1));
-halfReduction = ceil((length(eigen_values))/2);
-eigen_vector(:, halfReduction:end) = [];
-out_matrix = eigen_vector;
+
+
 end  % function
 
-function [ new_subspace ] = TransformNewSubSpace(data_matrix, eigen_matrix)
-% function: Input of data_matrix whether test or train set and eigen matrix
-% calculating the dot product will produced a new subspace
+function [ out_subspaceY ] = TransformNewSubSpaceY( in )
+% function: Short description
+%
+% Extended description
 
-new_subspace = dot(data_matrix, eigen_matrix);
 end  % function
